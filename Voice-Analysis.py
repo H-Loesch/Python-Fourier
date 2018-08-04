6a5052a400c7043f013a0ef8f5ebde1a1a47e8a4
H-Loesch <h38383838@tamu.edu>
2018-08-04 10:23:35 -0500

    Fourier Transform Working
    Forgot to commit this last night, but the fourier transform is working now. At least, it returns the proper output when given audio from a sine wave, so I'm counting that for right now.

--- Voice-Analysis.py
+++ Voice-Analysis.py
@@ -2,11 +2,13 @@
 #not sure if these category markers will actually be useful, but I guess we'll see. 
 
 #scipy import #scientific tools (fourier analysis, for example)
-import numpy #math things
+from numpy import fft
+import numpy
 import matplotlib.pylab as plt #plotting module
-
+from scipy.io import wavfile as wav #allows working with *.wav files in a numpy format.
 
 #Methods
+#//////////////////////////////////////////////////////////
 
 #plots a single x-y figure.
 #Also allows labelling of x and y axis, and an input string (all in that order)
@@ -20,15 +22,32 @@ def graphNow(labelX="X-axis", labelY="Y-axis"):
     plt.ylabel(labelY)
     plt.show()
 
-#data retrieval 
 
+
+#data retrieval 
+#//////////////////////////////////////////////////////////
+
+#reads from the given file, and returns an array of the sample rate, and an array of the data
+[sampleRate, aud_data] = wav.read("sinewave.wav") #these files should be at 32000Hz, 16-bit signed *.wav
+#print(sinewave[1])
 
 #data analysis/processing
+#//////////////////////////////////////////////////////////
+
+sampleNum = len(aud_data)
+sampleTimes = numpy.linspace(0, sampleNum / sampleRate, sampleNum) #creates array of sample timestamps
+sampleLength = sampleNum / sampleRate #Duration of audio signal (seconds)
+xAxis = numpy.arange(0, sampleNum, 1/sampleLength) #Dividing the unit step length by 1/sampleLength ensures the x-axis is scaled correctly
+
+fourierTransform = abs(fft.fft(aud_data))
 
 
 #data display
+#//////////////////////////////////////////////////////////
 
+plt.plot(xAxis[0:sampleNum], fourierTransform[0:sampleNum], 'b--') 
+#plt.plot(numpy.linspace(0, sampleNum, sampleNum), fft.fft(sampleNum * 5* numpy.sin(440 * numpy.pi * 2 * numpy.linspace(0, sampleNum, sampleNum))), 'r-')
+plt.show()
 
 #closing
-
-
+#//////////////////////////////////////////////////////////
