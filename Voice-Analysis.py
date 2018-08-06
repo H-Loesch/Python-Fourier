8ac2d23c1c4794e97811189f7b9e5be3b4efb441
H-Loesch <h38383838@tamu.edu>
2018-08-06 01:51:19 -0500

    More data recording
    Recording more data. Yes, I"m using github as a way to store research data. It's not like these graphs are high enough resolution to really make them useful.
    
    Also wrote down an idea for how voice comparison could work. It's extremely rough, but that might be fine.

--- Voice-Analysis.py
+++ Voice-Analysis.py
@@ -45,26 +45,34 @@ def plotFourier(sampleRate = 32000, fourierArray = [], size = 2):
     #sampleTimes = numpy.linspace(0, sampleNum / sampleRate, sampleNum) #array of sample timestamps. Not used, but may be useful?
     sampleLength = sampleNum / sampleRate #duration of audio signal (seconds)
     xAxis = numpy.arange(0, sampleNum, 1/sampleLength)
-    xAxis = xAxis[0:sampleNum-(sampleNum % size)] #ensures that this is divisble by size
+    xAxis = xAxis[0:sampleNum-(sampleNum % size)] #ensures that this is divisible by size
     
     fourier = abs(fourierArray)[0:(sampleNum - (sampleNum % size))] #make sure this is divisible by size. We're cutting off most so it's fine.
     fourier = numpy.split(fourier, size)[0]
     xAxis = numpy.split(xAxis, size)[0]
     plt.semilogx(xAxis, fourier)
 
+    peaks = []
+    print(sampleRate/len(xAxis)/size)
+    for i in range(3):
+        print(xAxis[i]*(size*len(xAxis)/sampleRate)/4) This loops over the xAxis at about .25Hz per cycle. 
+                                                       
+
+
+
 #int main() {
 #//////////////////////////////////////////////////////////////////////////////
 
-plt.subplot(2, 2, 1)
+plt.subplot(2, 1, 1)
 plotFourierFile("ASample1Clean.wav")
 plt.title("A's Sample 1") 
-
-plt.subplot(2, 2, 2)
+'''
+plt.subplot(2, 1, 2)
 plt.title("A's Sample 2") 
+#plt.xlabel("Frequency (Hz)")
 plotFourierFile("Asample2Clean.wav")
 
-
-plt.subplot(2, 2, 3)
+plt.subplot(2, 1, 2)
 plt.title("My sample 1")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Magnitude") 
@@ -75,5 +83,18 @@ plt.title("My sample 2")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Magnitude") 
 plotFourierFile("MSample2Clean.wav")
+'''
 
-plt.show()
+
+'''current ideas for comparison/identification method:
+find the peak of the fourier array
+store its frequency somewhere
+find the peak again, excluding the zone, like... 40Hz to either side of the old one. (or some amount like that)
+repeat until we have 3-4 peaks
+Then we can use the location of those peaks as a VERY crude identification of the speaker
+
+Please keep in mind I'm working on limited time, knowledge, and also this is my first draft of the idea, so. 
+I don't really have the time, resources, or audio equipment to really create a sophisticated or even good system for this.
+
+Range around each peak should be expanded based on the peak's frequency
+'''
